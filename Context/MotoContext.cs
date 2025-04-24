8c4dbeff201fda7ec74547831434f95bbe8ce8cc
Felipe Kal <felipe.kal@outlook.com>
2025-04-24 12:42:21 -0300

    first commit

---
+++ Context/MotoContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Context
+{
+    public class MotoContext : DbContext
+    {
+
+        public MotoContext(DbContextOptions<MotoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Moto> Motos {get; set;}
+        
+    }
+}
---
+++ Models/Moto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoMVC.Models
+{
+    public class Moto
+    {
+        public string Nome { get; set; }
+        public DateTime Data { get; set; }
+        public double Valor { get; set; }
+        public double Kilometragem { get; set; }
+        public double Litros { get; set; }
+        public string Tags { get; set; }
+        public string Onde { get; set; }
+        public double KmAndado { get; set; }
+        public double Media { get; set; }
+
+        
+    }
+}
